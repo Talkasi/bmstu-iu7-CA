30478c8ba1e663cd5962a15736ef20098b69d93b
Talkasi <kitalkasi@gmail.com>
2024-05-11 09:45:13 +0300

    FEAT: Newton interpolation added

---
+++ lab_02/newton.cpp
@@ -0,0 +1,80 @@
+#include "newton.hpp"
+#include "io.hpp"
+
+table_t get_nearest(table_t &table, int n, double x)
+{
+	table_t res = {};
+	int middle_i = -1;
+
+	for (int i = 0; i < table.size(); ++i) {
+		if (table[i][0] > x) {
+			middle_i = i;
+			break;
+		}
+	}
+
+	int li = middle_i - n / 2;
+	int ri = li + n - 1;
+
+	if (li < 0) {
+		li = 0;
+		ri = n - 1;
+	} else if (ri >= table.size()) {
+		ri = table.size() - 1;
+		li = ri - n + 1;
+	}
+
+	for (; li <= ri; ++li)
+		res.push_back(table[li]);
+
+	return res;
+}
+
+diff_table_row_t first_divided_difference(table_t &table)
+{
+	diff_table_row_t res;
+	for (int i = 0; i < table.size(); ++i)
+		res.push_back(table[i][1]);
+
+	return res;
+}
+
+diff_table_t newton_divided_difference(table_t &table)
+{
+	diff_table_t diff_table = {first_divided_difference(table)};
+
+	for (int i = 1; i < table.size(); ++i) {
+		diff_table_row_t tmp;
+		for (int j = 0; j < table.size() - i; ++j)
+			tmp.push_back((diff_table[i - 1][j + 1] - diff_table[i - 1][j]) / (table[i + j][0] - table[j][0]));
+
+		diff_table.push_back(tmp);
+	}
+
+	return diff_table;
+}
+
+double newton(table_t &table, int n, double x)
+{
+	table_t new_table = get_nearest(table, n + 1, x);
+	print_table(new_table);
+
+	diff_table_t diff_table = newton_divided_difference(new_table);
+
+	double res = diff_table[0][0];
+	double m = 1;
+
+	for (int i = 1; i <= n; ++i) {
+		m *= (x - new_table[i - 1][0]);
+		res += m * diff_table[i][0];
+	}
+
+	return res;
+}
+
+double newton_second_derivative(table_t &table, double x)
+{
+    table_t new_table = get_nearest(table, 4, x);
+    diff_table_t diff_table = newton_divided_difference(new_table);
+    return 2 * diff_table[2][0] + diff_table[3][0] * (6 * x - 2 * (new_table[0][0] + new_table[1][0] + new_table[2][0]));
+}
---
+++ lab_02/newton.hpp
@@ -0,0 +1,15 @@
+#ifndef NEWTON_HPP
+#define NEWTON_HPP
+#include <vector>
+#include "table.h"
+
+using diff_table_row_t = std::vector<double>;
+using diff_table_t = std::vector<diff_table_row_t>;
+
+diff_table_row_t first_divided_difference(table_t &table);
+diff_table_t newton_divided_difference(table_t &table);
+
+double newton(table_t &table, int n, double x);
+double newton_second_derivative(table_t &table, double x);
+
+#endif
