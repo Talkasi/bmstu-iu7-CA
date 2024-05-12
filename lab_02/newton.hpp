9f15644825c8637edbca59198d143837ff447834
Talkasi <kitalkasi@gmail.com>
2024-05-12 12:02:47 +0300

    FIX: Bug with working table

--- lab_02/newton.hpp
+++ lab_02/newton.hpp
@@ -1,7 +1,7 @@
 #ifndef NEWTON_HPP
 #define NEWTON_HPP
-#include <vector>
 #include "table.h"
+#include <vector>
 
 using diff_table_row_t = std::vector<double>;
 using diff_table_t = std::vector<diff_table_row_t>;
@@ -9,7 +9,7 @@ using diff_table_t = std::vector<diff_table_row_t>;
 diff_table_row_t first_divided_difference(table_t &table);
 diff_table_t newton_divided_difference(table_t &table);
 
-double newton(table_t &table, int n, double x);
+double newton(table_t &table, int n, double x, bool verbose);
 double newton_second_derivative(table_t &table, double x);
 
-#endif
+#endif // NEWTON_HPP
